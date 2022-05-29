dc0a4ab4adeeee40231e3430306117f73d2acff9
SabbirSami <smd71430@gmail.com>
2022-05-29 22:23:48 +0600

    Types with usestate and event handler

--- src/Conmponent/Counter.tsx
+++ src/Conmponent/Counter.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
+interface User {
+    name: string,
+    job: string
+}
+
 
 const Counter = () => {
     const [counter, setCounter] = useState<number>(0);
+    const [user, setUser] = useState<User |>(null);
     const handleIncrease = (): void => {
         setCounter(counter + 1);
     };
     return (
         <div>
             <h1>Counter</h1>
-            <h2>0</h2>
+            <h2>{counter}</h2>
             <button onClick={handleIncrease}>Increase</button>
         </div>
     );
