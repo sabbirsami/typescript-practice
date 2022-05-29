bc59b82fe2dcd106d1a8aad295f1c6ebf21aa8b5
SabbirSami <smd71430@gmail.com>
2022-05-29 18:35:03 +0600

    Basic types number string

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let student: string = "sami";
+let age: number = 18;
+let isSmart: boolean = true;
+let students: string[] = ["joe", "biden", "harris"];
+let fees: number[] = [12, 45, 86, 32];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
